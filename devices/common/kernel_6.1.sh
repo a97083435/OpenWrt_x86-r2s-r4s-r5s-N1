ec322a1886175015296a38c20e166c0ff0278586
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-03-24 07:15:10 +0800

    update

--- devices/common/kernel_6.1.sh
+++ devices/common/kernel_6.1.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-rm -rf target/linux package/kernel package/boot package/firmware/linux-firmware
+rm -rf target/linux package/kernel package/boot package/firmware/linux-firmware package/network/config/wifi-scripts
 
 mkdir new; cp -rf .git new/.git
 cd new
 git reset --hard origin/master
 
-cp -rf --parents target/linux package/kernel package/boot package/firmware/linux-firmware include/kernel-6.1 ../
+cp -rf --parents target/linux package/kernel package/boot package/firmware/linux-firmware include/kernel-6.1 package/network/config/wifi-scripts ../
 cd -
 
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/hack-6.1
-
 
 mkdir package/kernel/mt76/patches
 curl -sfL https://raw.githubusercontent.com/immortalwrt/immortalwrt/master/package/kernel/mt76/patches/0001-mt76-allow-VHT-rate-on-2.4GHz.patch -o package/kernel/mt76/patches/0001-mt76-allow-VHT-rate-on-2.4GHz.patch
